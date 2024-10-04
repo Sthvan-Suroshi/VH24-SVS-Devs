d6c815b85cf01cfb6f7844f2818119529cf06b8c
Swapnil0809 <swapniljadhav22811@gmail.com>
2024-10-04 16:51:55 +0530

    add registration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import Registration from './components/Registration';
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Registration />} />
 
+
+
+
+        
       </Routes>
     </>
 
