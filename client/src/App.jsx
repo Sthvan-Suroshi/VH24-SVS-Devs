2c513ae12677232a956623f545be7193207cf7a3
Swapnil0809 <swapniljadhav22811@gmail.com>
2024-10-05 01:24:05 +0530

    added components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Registration from './pages/RegistrationForm';
+import Institution from './components/institution';
+import Shopkeeper from './components/shopkeeper';
+import Donor from './components/donor';
+import Feedback from './components/feedback';
+import Inslanding from './components/InstituteLandingPage';
+
+
+
 
 
 function App() {
@@ -12,6 +20,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Registration />} />
+        <Route path="/institution" element={<Institution />} />
+        <Route path="/shopkeeper" element={<Shopkeeper />} />
+        <Route path="/donor" element={<Donor />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/landing" element={<Inslanding />} />
+
+
+
 
 
 
