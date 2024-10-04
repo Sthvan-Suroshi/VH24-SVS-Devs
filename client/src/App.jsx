7f6d39f7b4d9ea58b11bd910e26cd5aeedfe17d3
Sthvan-Suroshi <123sthavansuroshi@gmail.com>
2024-10-04 10:04:56 +0530

    setup commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-red-300 h-screen">
+      App
+    </div>
+  );
+}
+
+export default App;
