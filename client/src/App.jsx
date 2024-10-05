5068b3a03bb19d1b09c320b47427e0cac3ce74d6
Swapnil0809 <swapniljadhav22811@gmail.com>
2024-10-05 09:27:50 +0530

    updated pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home";
 import Registration from "./pages/RegistrationForm";
-import Institution from "./components/institution";
-import Shopkeeper from "./components/shopkeeper";
-import Donor from "./components/donor";
-import Feedback from "./components/feedback";
 import LoginComponent from "./components/Login";
 import Institution from "./pages/institution";
 import Shopkeeper from "./pages/shopkeeper";
@@ -20,10 +17,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Registration />} />
-        <Route path="/institution" element={<Institution />} />
-        <Route path="/shopkeeper" element={<Shopkeeper />} />
-        <Route path="/donor" element={<Donor />} />
-        <Route path="/feedback" element={<Feedback />} />
         <Route path="/login" element={<LoginComponent />} />
         <Route path="/institution" element={<Institution />} />
         <Route path="/shopkeeper" element={<Shopkeeper />} />
